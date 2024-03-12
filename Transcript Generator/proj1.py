014d749748992f294a714b9ea40e3969e9481c91
Sahil Chaudhari <76389534+Lord2709@users.noreply.github.com>
2024-03-12 14:28:44 +0530

    Libraries imported

---
+++ Transcript Generator/proj1.py
@@ -0,0 +1,12 @@
+import csv
+import os
+from fpdf import FPDF
+'''The FPDF library is a PHP class that allows you to generate PDF files with pure PHP, 
+without using the PDFlib library1. It is free to use and modify, and it has many 
+features such as image support, colors, links, fonts, page compression, etc'''
+
+from pywebio import *
+from pywebio.input import *
+from pywebio.output import *
+'''pywebio: A Python package that allows you to write interactive web applications or 
+browser-based GUI applications without the need to have knowledge of HTML and JS'''
